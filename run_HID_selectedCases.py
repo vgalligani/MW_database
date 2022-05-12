a91953e6b6f30166a7b029bce46ac0094c36d18a
Victoria Sol Galligani <victoria.galligani@cima.fcen.uba.ar>
2022-05-12 12:40:28 -0300

    Update run_HID_selectedCases.py

--- run_HID_selectedCases.py
+++ run_HID_selectedCases.py
@@ -468,7 +468,7 @@ def smooth_and_trim(x, window_len=11, window='hanning'):
     return y[int(window_len / 2):len(x) + int(window_len / 2)]
 
 #------------------------------------------------------------------------------  
-def plot_test_ppi(radar, phi_corr, lat_pf, lon_pf):
+def plot_test_ppi(radar, phi_corr, lat_pf, lon_pf, general_title):
     
     start_index = radar.sweep_start_ray_index['data'][0]
     end_index   = radar.sweep_end_ray_index['data'][0]
@@ -492,8 +492,9 @@ def plot_test_ppi(radar, phi_corr, lat_pf, lon_pf):
     cbar.cmap.set_under(under)
     cbar.cmap.set_over(over)
     axes[0,0].grid(True)
-    axes[0,0].plot(lon_pf, lat_pf, marker='o', markersize=60, markerfacecolor="None",
-         markeredgecolor='black', markeredgewidth=2) 
+    axes[0,0].plot(lon_pf, lat_pf, marker='*', markersize=20, markerfacecolor="None",
+         markeredgecolor='black', markeredgewidth=2, label='GMI(PF) center') 
+    axes[0,0].legend()
     #-- RHOHV
     [units, cmap, vmin, vmax, max, intt, under, over] = set_plot_settings('rhohv')
     pcm1 = axes[0,1].pcolormesh(lons, lats, rhv, cmap=cmap, vmin=vmin, vmax=vmax)
@@ -515,6 +516,8 @@ def plot_test_ppi(radar, phi_corr, lat_pf, lon_pf):
     cbar.cmap.set_under(under)
     cbar.cmap.set_over(over)
     axes[1,1].grid(True)
+    
+    plt.suptitle(general_title, fontsize=14)
 
     return
 #------------------------------------------------------------------------------  
@@ -2045,7 +2048,8 @@ if __name__ == '__main__':
     
     
 
-    
+    era5_dir       = '/home/victoria.galligani/Work/Studies/Hail_MW/ERA5/'
+
     
     
     # --- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ----- ---- ---- ----    
@@ -2053,18 +2057,63 @@ if __name__ == '__main__':
     # CASO SUPERCELDA: note contours of interest (coi) input to plot_gmi. Selected manually. using the functions inside plot_gmi. 
     lon_pfs = [-64.80]
     lat_pfs = [-31.83]
+    time_pfs = '2058'
     phail   = [0.534]
     MIN85PCT = [131.1081]
     MIN37PCT = [207.4052]
     #
-    rfile = 'cfrad.20180208_205455.0000_to_20180208_205739.0000_RMA1_0201_02.nc'
-    gfile = '1B.GPM.GMI.TB2016.20180208-S193936-E211210.022436.V05A.HDF5'
+    rfile     = 'cfrad.20180208_205455.0000_to_20180208_205739.0000_RMA1_0201_02.nc'
+    gfile     = '1B.GPM.GMI.TB2016.20180208-S193936-E211210.022436.V05A.HDF5'
+    era5_file = ''
+    #
     opts = {'xlim_min': -66, 'xlim_max': -62, 'ylim_min': -33, 'ylim_max': -30}
+    #
+    radar = pyart.io.read('/home/victoria.galligani/Work/Studies/Hail_MW/radar_data/'+'RMA1/'+rfile)
+    sys_phase  = pyart.correct.phase_proc.det_sys_phase(radar, ncp_lev=0.8, rhohv_lev=0.8, ncp_field='RHOHV', rhv_field='RHOHV', phidp_field='PHIDP')
+    corr_phidp = correct_phidp(radar.fields['PHIDP']['data'], radar.fields['RHOHV']['data'], 0, 280)
+    plot_test_ppi(radar , corr_phidp, lat_pfs[0], lon_pfs[0], 'radar at '+rfile[15:19]+' UTC and PF at '+time_pfs+' UTC')
     plot_gmi('/home/victoria.galligani/Work/Studies/Hail_MW/GMI_data/'+gfile, opts,
                                   '/home/victoria.galligani/Work/Studies/Hail_MW/radar_data/'+'RMA1/', rfile, lon_pfs, lat_pfs, coi=[1,2,3])
     plt.title('GMI'+gfile[18:26]+' Phail='+str(phail[0])+' MIN85PCT: '+str(MIN85PCT[0])+' MIN37PCT:'+str(MIN37PCT[0]))
     # Inside radar PCTs (que en principio son PFs). look at TB distribution w/ MIN85PCT and MIN37PCT.  
+    [lon_inside, lat_inside, lon_inside2, lat_inside2, tb_s1_cont_2, tb_s2_cont_2] = return_gmi_inside_contours(
+        '/home/victoria.galligani/Work/Studies/Hail_MW/GMI_data/'+gfile, opts,
+                                   '/home/victoria.galligani/Work/Studies/Hail_MW/radar_data/'+'RMA1/', rfile, lon_pfs, lat_pfs, 3)
+    # Test minPCTs: 
+    PCT37 = (2.15 * tb_s1_cont_2[:,5]) - (1.15 * tb_s1_cont_2[:,6]) # == 184.39   NOT 207.40! perhaps use optimal Table 3 Cecil(2018)? 1.2 increases minTB
+    PCT89 = 1.7  * tb_s1_cont_2[:,7] - 0.7  * tb_s1_cont_2[:,8] # == 131.03
+    # Plot RHIs w/ corrected ZDR, first calculate freezing level:
+    ERA5_field = xr.load_dataset(era5_dir+era5_file, engine="cfgrib")
+    elemj      = find_nearest(ERA5_field['latitude'], PFs_lat[i])
+    elemk      = find_nearest(ERA5_field['longitude'], PFs_lon[i])
+    tfield_ref = ERA5_field['t'][:,elemj,elemk] - 273 # convert to C
+    geoph_ref  = (ERA5_field['z'][:,elemj,elemk])/9.80665
+    # Covert to geop. height (https://confluence.ecmwf.int/display/CKB/ERA5%3A+compute+pressure+and+geopotential+on+model+levels%2C+geopotential+height+and+geometric+height)
+    Re         = 6371*1e3
+    alt_ref    = (Re*geoph_ref)/(Re-geoph_ref)
+    freezing_lev = np.array(alt_ref[find_nearest(tfield_ref, 0)]/1e3) 
+    #    
+    plot_rhi_RMA(rfile, '/home/victoria.galligani/Work/Studies/Hail_MW/radar_figures', '/home/victoria.galligani/Work/Studies/Hail_MW/radar_data/'+'RMA1/',
+                 'RMA1', 0, 150, 356, 4, freezing_lev)
+    plot_rhi_RMA(rfile, '/home/victoria.galligani/Work/Studies/Hail_MW/radar_figures', '/home/victoria.galligani/Work/Studies/Hail_MW/radar_data/'+'RMA1/',
+                 'RMA1', 0, 150, 220, 4, freezing_lev)
+    #
     
-    PCT37 = 2.15 * tb_s1_cont_2[:,5] - 1.15 * tb_s1_cont_2[:,6] == 184.39 NOT ==MIN37PCT?
-    PCT89 = 1.7  * tb_s1_cont_2[:,7] - 0.7  * tb_s1_cont_2[:,8] == 131.03
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     
