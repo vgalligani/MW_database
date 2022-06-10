bdee8dae1c14e803126ba134dbc609b9a43fd62e
Victoria Sol Galligani <victoria.galligani@cima.fcen.uba.ar>
2022-06-10 15:00:53 -0300

    Update mapping.py

--- mapping.py
+++ mapping.py
@@ -1,6 +1,317 @@
 from matplotlib import cm;
+from os import listdir
+import pyart
+import matplotlib.pyplot as plt
+import numpy as np
+from os.path import exists
+from shapely.geometry import Polygon
+import matplotlib
+import h5py
+import matplotlib.gridspec as gridspec
+import cartopy.crs as ccrs
+import cartopy.io.shapereader as shpreader
+import platform
+import cartopy.feature as cfeature
+from matplotlib.colors import ListedColormap
+from cartopy.mpl.ticker import LongitudeFormatter, LatitudeFormatter
+from os.path import isfile, join
+import pandas as pd
+from pyart.correct import phase_proc
+import xarray as xr
+from pyart.core.transforms import antenna_to_cartesian
+from copy import deepcopy
+from csu_radartools import csu_fhc
+import matplotlib.colors as colors
+import wradlib as wrl    
+from scipy.spatial import ConvexHull, convex_hull_plot_2d
+from matplotlib.path import Path
+from cycler import cycler
+
+#------------------------------------------------------------------------------
+def set_plot_settings(var): 
+    """
+    -------------------------------------------------------------
+    
+    -------------------------------------------------------------
+    OUT    [units, cmap, vmin, vmax, mac, int, under] todas de importancia
+                    para colormpas de las variables del radar.      
+    IN     var      Zhh, Zdr, Kdp, Ah, phidp, rhohv, Doppler Vel. 
+    ------
+    notes: mas colormaps en https://github.com/jjhelmus/pyart_colormaps_and_limits 
+    """
+    if var == 'Zhh':
+        units = 'Reflectivity [dBZ]'
+        cmap = colormaps('ref')
+        vmin = 0
+        vmax = 60
+        max = 60.01
+        intt = 5
+        under = 'white'
+        over = 'white'
+    elif var == 'Zhh_2':
+        units = 'Zh [dBZ]'
+        cmap = colormaps('ref2')
+        vmin = 0
+        vmax = 60
+        max = 60.01
+        intt = 5
+        under = 'white'
+        over = 'black'
+    elif var == 'Zdr':
+        units = 'ZDR [dBZ]'
+        vmin = -2
+        vmax = 10
+        max = 10.01
+        intt = 1
+        N = (vmax-vmin)/intt
+        cmap = discrete_cmap(int(N), 'jet') # colormaps('zdr')
+        under = 'white'
+        over = 'white'
+    elif var == 'Kdp':
+        units = 'KDP [deg/km]'
+        vmin = -0.1
+        vmax = 0.7
+        max = 0.71
+        intt = 0.1
+        N = (vmax-vmin)/intt
+        cmap = discrete_cmap(10, 'jet')
+        under = 'white'
+        over = 'black'
+    elif var == 'Ah':
+        units = '[dB/km]'
+        vmin = 0
+        vmax = 0.5
+        max = 0.51
+        intt = 0.05
+        N = (vmax-vmin)/intt
+        cmap = discrete_cmap(N, 'brg_r')
+        under = 'black'
+        over = 'white'
+    elif var == 'phidp':
+        units = 'PHIDP [deg]'
+        vmin = 0
+        vmax = 360
+        max = 360.1
+        intt = 10
+        N = (vmax-vmin)/intt
+        print(N)
+        cmap = discrete_cmap(int(N), 'jet')
+        under = 'white'
+        over = 'white'
+    elif var == 'rhohv':
+        units = r'$\rho_{hv}$'
+        vmin = 0.5
+        vmax = 1.
+        max = 1.01
+        intt = 0.1
+        N = round((vmax-vmin)/intt)
+        cmap = pyart.graph.cm.RefDiff #fun.discrete_cmap(N, 'jet')
+        under = 'white'
+        over = 'white'
+    else: #Doppler por ejemplo
+        units = r'V$_r$ [m/s]'
+        cmap = pyart.graph.cm.BuDRd18
+        vmin = -10
+        vmax = 10
+        max = 10.01
+        intt = 2
+        under = 'white'
+        over = 'white'    
+        
+    return units, cmap, vmin, vmax, max, intt, under, over
+
+#------------------------------------------------------------------------------  
+def colormaps(variable):
+    """
+    Choose colormap for a radar variable
+    variable : str
+       Radar variable to define which colormap to use. (e.g. ref,
+       dv, zdr..) More to come
+    returns : matplotlib ColorList
+       A Color List for the specified *variable*.
+    """
+    import matplotlib.colors as colors
+
+    # Definicion de las disntitas paletas de colores:
+    nws_ref_colors =([ [ 1.0/255.0, 159.0/255.0, 244.0/255.0],
+                    [  3.0/255.0,   0.0/255.0, 244.0/255.0],
+                    [  2.0/255.0, 253.0/255.0,   2.0/255.0],
+                    [  1.0/255.0, 197.0/255.0,   1.0/255.0],
+                    [  0.0/255.0, 142.0/255.0,   0.0/255.0],
+                    [253.0/255.0, 248.0/255.0,   2.0/255.0],
+                    [229.0/255.0, 188.0/255.0,   0.0/255.0],
+                    [253.0/255.0, 149.0/255.0,   0.0/255.0],
+                    [253.0/255.0,   0.0/255.0,   0.0/255.0],
+                    #[212.0/255.0,   0.0/255.0,   0.0/255.0, 0.4],
+                    [188.0/255.0,   0.0/255.0,   0.0/255.0],
+                    [248.0/255.0,   0.0/255.0, 253.0/255.0],
+                    [152.0/255.0,  84.0/255.0, 198.0/255.0]
+                    ])
+    # Definicion de las disntitas paletas de colores:
+    nws_ref_colors_transparent =([ [ 1.0/255.0, 159.0/255.0, 244.0/255.0, 0.3],
+                    [  3.0/255.0,   0.0/255.0, 244.0/255.0, 0.3],
+                    [  2.0/255.0, 253.0/255.0,   2.0/255.0, 0.3],
+                    [  1.0/255.0, 197.0/255.0,   1.0/255.0, 0.3],
+                    [  0.0/255.0, 142.0/255.0,   0.0/255.0, 0.3],
+                    [253.0/255.0, 248.0/255.0,   2.0/255.0, 0.3],
+                    [229.0/255.0, 188.0/255.0,   0.0/255.0, 1],
+                    [253.0/255.0, 149.0/255.0,   0.0/255.0, 1],
+                    [253.0/255.0,   0.0/255.0,   0.0/255.0, 1],
+                    #[212.0/255.0,   0.0/255.0,   0.0/255.0, 0.4],
+                    [188.0/255.0,   0.0/255.0,   0.0/255.0, 1],
+                    [248.0/255.0,   0.0/255.0, 253.0/255.0, 1],
+                    [152.0/255.0,  84.0/255.0, 198.0/255.0, 1]
+                    ])
+    
+    nws_zdr_colors = ([ [  1.0/255.0, 159.0/255.0, 244.0/255.0],
+                    [  3.0/255.0,   0.0/255.0, 244.0/255.0],
+                    [  2.0/255.0, 253.0/255.0,   2.0/255.0],
+                    [  1.0/255.0, 197.0/255.0,   1.0/255.0],
+                    [  0.0/255.0, 142.0/255.0,   0.0/255.0],
+                    [253.0/255.0, 248.0/255.0,   2.0/255.0],
+                    [229.0/255.0, 188.0/255.0,   2.0/255.0],
+                    [253.0/255.0, 149.0/255.0,   0.0/255.0],
+                    [253.0/255.0,   0.0/255.0,   0.0/255.0],
+                    [188.0/255.0,   0.0/255.0,   0.0/255.0],
+                    [152.0/255.0,  84.0/255.0, 198.0/255.0]
+                    ])
+
+    nws_dv_colors = ([  [0,  1,  1],
+                    [0,  0.966666638851166,  1],
+                    [0,  0.933333337306976,  1],
+                    [0,  0.899999976158142,  1],
+                    [0,  0.866666674613953,  1],
+                    [0,  0.833333313465118,  1],
+                    [0,  0.800000011920929,  1],
+                    [0,  0.766666650772095,  1],
+                    [0,  0.733333349227905,  1],
+                    [0,  0.699999988079071,  1],
+                    [0,  0.666666686534882,  1],
+                    [0,  0.633333325386047,  1],
+                    [0,  0.600000023841858,  1],
+                    [0,  0.566666662693024,  1],
+                    [0,  0.533333361148834,  1],
+                    [0,  0.5,  1],
+                    [0,  0.466666668653488,  1],
+                    [0,  0.433333337306976,  1],
+                    [0,  0.400000005960464,  1],
+                    [0,  0.366666674613953,  1],
+                    [0,  0.333333343267441,  1],
+                    [0,  0.300000011920929,  1],
+                    [0,  0.266666680574417,  1],
+                    [0,  0.233333334326744,  1],
+                    [0,  0.200000002980232,  1],
+                    [0,  0.16666667163372,   1],
+                    [0,  0.133333340287209,  1],
+                    [0,  0.100000001490116,  1],
+                    [0,  0.0666666701436043, 1],
+                    [0,  0.0333333350718021, 1],
+                    [0,  0,  1],
+                    [0,  0,  0],
+                    [0,  0,  0],
+                    [0,  0,  0],
+                    [0,  0,  0],
+                    [1,  0,  0],
+                    [1,  0.0322580635547638, 0],
+                    [1,  0.0645161271095276, 0],
+                    [1,  0.0967741906642914, 0],
+                    [1,  0.129032254219055,  0],
+                    [1,  0.161290317773819,  0],
+                    [1,  0.193548381328583,  0],
+                    [1,  0.225806444883347,  0],
+                    [1,  0.25806450843811,   0],
+                    [1,  0.290322571992874,  0],
+                    [1,  0.322580635547638,  0],
+                    [1,  0.354838699102402,  0],
+                    [1,  0.387096762657166,  0],
+                    [1,  0.419354826211929,  0],
+                    [1,  0.451612889766693,  0],
+                    [1,  0.483870953321457,  0],
+                    [1,  0.516129016876221,  0],
+                    [1,  0.548387110233307,  0],
+                    [1,  0.580645143985748,  0],
+                    [1,  0.612903237342834,  0],
+                    [1,  0.645161271095276,  0],
+                    [1,  0.677419364452362,  0],
+                    [1,  0.709677398204803,  0],
+                    [1,  0.74193549156189,   0],
+                    [1,  0.774193525314331,  0],
+                    [1,  0.806451618671417,  0],
+                    [1,  0.838709652423859,  0],
+                    [1,  0.870967745780945,  0],
+                    [1,  0.903225779533386,  0],
+                    [1,  0.935483872890472,  0],
+                    [1,  0.967741906642914,  0],
+                    [1,  1,  0]   ])
 
 
+    cmap_nws_ref = colors.ListedColormap(nws_ref_colors)
+    cmap_nws_zdr = colors.ListedColormap(nws_zdr_colors)
+    cmap_nws_dv = colors.ListedColormap(nws_dv_colors)
+    cmap_nws_ref_trans = colors.ListedColormap(nws_ref_colors_transparent)
+    
+
+    if variable == 'ref':
+       return cmap_nws_ref
+    if variable == 'ref2':
+       return cmap_nws_ref_trans
+        
+
+    if variable == 'zdr':
+       return cmap_nws_zdr
+
+    if variable == 'dv':
+       return cmap_nws_dv
+
+#------------------------------------------------------------------------------  
+def pyplot_rings(lat_radar,lon_radar,radius):
+    """
+    Calculate lat-lon of the maximum range ring
+    lat_radar : float32
+       Radar latitude. Positive is north.
+    lon_radar : float32
+       Radar longitude. Positive is east.
+    radius : float32
+       Radar range in kilometers.
+    returns : numpy.array
+       A 2d array containing the 'radius' range latitudes (lat) and longitudes (lon)
+    """
+    import numpy as np
+
+    R=12742./2.
+    m=2.*np.pi*R/360.
+    alfa=np.arange(-np.pi,np.pi,0.0001)
+
+    nazim  = 360.0
+    nbins  = 480.0
+    binres = 0.5
+
+    #azimuth = np.transpose(np.tile(np.arange(0,nazim,1), (int(nbins),1)))
+    #rangos  = np.tile(np.arange(0,nbins,1)*binres, (int(nazim),1))
+    #lats    = lat_radar + (rangos/m)*np.cos((azimuth)*np.pi/180.0)
+    #lons    = lon_radar + (rangos/m)*np.sin((azimuth)*np.pi/180.0)/np.cos(lats*np.pi/180)
+
+    lat_radius = lat_radar + (radius/m)*np.sin(alfa)
+    lon_radius = lon_radar + ((radius/m)*np.cos(alfa)/np.cos(lat_radius*np.pi/180))
+
+    return lat_radius, lon_radius
+
+#------------------------------------------------------------------------------  
+def discrete_cmap(N, base_cmap=None):
+    """Create an N-bin discrete colormap from the specified input map"""
+
+    # Note that if base_cmap is a string or None, you can simply do
+    #    return plt.cm.get_cmap(base_cmap, N)
+    # The following works for string, None, or a colormap instance:
+
+    import matplotlib.pyplot as plt
+    import numpy as np
+
+    base = plt.cm.get_cmap(base_cmap)
+    color_list = base(np.linspace(0, 1, N))
+    cmap_name = base.name + str(N)
+    
+    return base.from_list(cmap_name, color_list, N)
 
 #------------------------------------------------------------------------------  
 def find_nearest(array, value):
@@ -40,6 +351,14 @@ def plot_Zhppi_wGMIcontour(radar, lat_pf, lon_pf, general_title, fname, nlev, op
       lat_gmi[np.where(lat_gmi[:,j] <=  options['ylim_min']-5),:] = np.nan  
       lon_gmi[np.where(lat_gmi[:,j] >=  options['ylim_max']+5),:] = np.nan
       lon_gmi[np.where(lat_gmi[:,j] <=  options['ylim_min']-5),:] = np.nan  	
+	
+    # keep domain of interest only by keeping those where the center nadir obs is inside domain
+    inside_s1   = np.logical_and(np.logical_and(lon_gmi >= options['xlim_min'], lon_gmi <=  options['xlim_max']), 
+                              np.logical_and(lat_gmi >= options['ylim_min'], lat_gmi <= options['ylim_max']))
+    inside_s2   = np.logical_and(np.logical_and(lon_s2_gmi >= options['xlim_min'], lon_s2_gmi <=  options['xlim_max']), 
+                                         np.logical_and(lat_s2_gmi >= options['ylim_min'], lat_s2_gmi <= options['ylim_max']))    
+    lon_gmi_inside   =  lon_gmi[inside_s1] 
+    lat_gmi_inside   =  lat_gmi[inside_s1] 	
 	
     PCT89 = 1.7  * tb_s1_gmi[:,:,7] - 0.7  * tb_s1_gmi[:,:,8] 
     
@@ -95,13 +414,14 @@ def plot_Zhppi_wGMIcontour(radar, lat_pf, lon_pf, general_title, fname, nlev, op
     	array_points = np.array(vertices)
     	##--- Run hull_paths and intersec
     	hull_path   = Path( array_points[convexhull.vertices] )
-    	datapts = np.column_stack((np.ravel(lon_gmi[1:,:]),np.ravel(lat_gmi[1:,:])))
+        datapts = np.column_stack((lon_gmi_inside,lat_gmi_inside))
+	#datapts = np.column_stack((np.ravel(lon_gmi[1:,:]),np.ravel(lat_gmi[1:,:])))
 	if ii==0:
-		inds1 = hull_path.contains_points(datapts)
+		inds_1 = hull_path.contains_points(datapts)
 	if ii==1:
-		inds2 = hull_path.contains_points(datapts)
-	if ii==3:
-		inds3 = hull_path.contains_points(datapts)
+		inds_2 = hull_path.contains_points(datapts)
+	if ii==2
+		inds_3 = hull_path.contains_points(datapts)
 
     plt.xlim([options['xlim_min'], options['xlim_max']])
     plt.ylim([options['ylim_min'], options['ylim_max']])
@@ -196,19 +516,27 @@ def plot_Zhppi_wGMIcontour(radar, lat_pf, lon_pf, general_title, fname, nlev, op
     axes[1].plot(lon_radius, lat_radius, 'k', linewidth=0.8) 
     axes[0].plot(lon_radius, lat_radius, 'k', linewidth=0.8) 
     # Add labeled contours of cois of interest! 
-    axes[0].plot(np.ravel(lon_gmi[1:,:])[inds_1[0]], np.ravel(lat_gmi[1:,:])[inds_1[0]], '*', markersize=40)
-    axes[1].plot(np.ravel(lon_gmi[1:,:])[inds_1[0]], np.ravel(lat_gmi[1:,:])[inds_1[0]], '*', markersize=40)
-    if ii == 2:
-	axes[0].plot(np.ravel(lon_gmi[1:,:])[inds_2[0]], np.ravel(lat_gmi[1:,:])[inds_2[0]], 'k*', markersize=40)
-	axes[1].plot(np.ravel(lon_gmi[1:,:])[inds_2[0]], np.ravel(lat_gmi[1:,:])[inds_2[0]], 'k*', markersize=40)
-    if ii == 3:
-	axes[0].plot(np.ravel(lon_gmi[1:,:])[inds_2[0]], np.ravel(lat_gmi[1:,:])[inds_2[0]], '*', color='darkblue', markersize=40)
-	axes[0].plot(np.ravel(lon_gmi[1:,:])[inds_3[0]], np.ravel(lat_gmi[1:,:])[inds_3[0]], '*', markersize=40)
-	axes[1].plot(np.ravel(lon_gmi[1:,:])[inds_2[0]], np.ravel(lat_gmi[1:,:])[inds_2[0]], '*', markersize=40)
-	axes[1].plot(np.ravel(lon_gmi[1:,:])[inds_3[0]], np.ravel(lat_gmi[1:,:])[inds_3[0]], '*', markersize=40)
-	
-    #axes[0].plot(np.nan, np.nan, '*', markersize=40)
+    axes[0].plot(lon_gmi_inside[inds_1], lat_gmi_inside[inds_1], 'o', markersize=20, markerfacecolor='black')
+    axes[1].plot(lon_gmi_inside[inds_1], lat_gmi_inside[inds_1], 'o', markersize=20, markerfacecolor='black')
+    if ii == 1
+	axes[0].plot(lon_gmi_inside[inds_2], at_gmi_inside[inds_2], 'o', markersize=20, markerfacecolor='darkblue')
+	axes[1].plot(lon_gmi_inside[inds_2], lat_gmi_inside[inds_2], 'o', markersize=200, markerfacecolor='darkblue')
+    if ii == 2
+	axes[0].plot(lon_gmi_inside[inds_2], lat_gmi_inside[inds_2], 'o', markersize=20, markerfacecolor='darkblue')
+	axes[0].plot(lon_gmi_inside[inds_3], lat_gmi_inside[inds_3], 'o', markersize=20, markerfacecolor='darkred')
+	axes[1].plot(lon_gmi_inside[inds_2], lat_gmi_inside[inds_2], 'o', markersize=20, markerfacecolor='darkblue')
+	axes[1].plot(lon_gmi_inside[inds_3], lat_gmi_inside[inds_3], 'o', markersize=20, markerfacecolor='darkred')
 
+    axes[0].hold(True)
+    dummy = axes[0].plot(np.nan, np.nan, 'o', markersize=20, markerfacecolor='black', label='icoi:'+str(icoi[0]))
+    axes[0].legend(dummy, +str(icoi[0]))
+    if ii == 1:
+	dummy = [axes[0].plot([], [], 'o', markersize=20, markerfacecolor=cc)[0] for cc in ['black','darkblue']]
+	axes[0].legend(dummy, ['icoi:'+str(icoi[0]), 'icoi:'+str(icoi[1])])
+    if ii == 2:
+	dummy = [axes[0].plot([], [], 'o', markersize=20, markerfacecolor=cc)[0] for cc in ['black','darkblue','darkred']]
+	axes[0].legend(dummy, ['icoi:'+str(icoi[0]), 'icoi:'+str(icoi[1]), 'icoi:'+str(icoi[2])])	
+		 
     return
 
 #------------------------------------------------------------------------------  
@@ -387,7 +715,7 @@ def plot_3D_Zhppi(radar, lat_pf, lon_pf, general_title, fname, nlev, options):
     radar_1 = pyart.io.read('/home/victoria.galligani/Work/Studies/Hail_MW/radar_data/'+'RMA1/'+rfile_1)
     radar_2 = pyart.io.read('/home/victoria.galligani/Work/Studies/Hail_MW/radar_data/'+'RMA1/'+rfile_2)
     plot_Zhppi_wGMIcontour(radar, lat_pfs, lon_pfs, 'radar at '+rfile[15:19]+' UTC and PF at '+time_pfs[0]+' UTC', 
-                           gmi_dir+gfile, 0, opts, era5_dir+era5_file, icoi=3)
+                           gmi_dir+gfile, 0, opts, era5_dir+era5_file, icoi=[3,4])
     #-------------------------- 
     # en el gridded se pueden hacer vertical slices? 
     grided = pyart.map.grid_from_radars(radar, grid_shape=(20, 470, 470), 
