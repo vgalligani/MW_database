7d9845db1b29e36fa068d738a9e84abd3e561da9
Victoria Sol Galligani <victoria.galligani@cima.fcen.uba.ar>
2022-07-28 15:53:42 -0300

    Update mapping.py

--- mapping.py
+++ mapping.py
@@ -2634,39 +2634,8 @@ def plot_HID_PPI(radar, options, nlev, azimuth_ray, diff_value, tfield_ref, alt_
     PHIDP = radar.fields['PHIDP']['data'][start_index:end_index]
     KDP   = radar.fields['KDP']['data'][start_index:end_index]
     ZH    = radar.fields['TH']['data'][start_index:end_index]
+    RHIs_nlev = radar.fields['HID']['data'][start_index:end_index]
 
-    #----------------------------------------------------------------------
-    #- Figure of RHIs (at first six elevations
-    #---------------------------------------- HID
-    hid_colors = ['MediumBlue', 'DarkOrange', 'LightPink',
-              'Cyan', 'DarkGray', 'Lime', 'Yellow', 'Red', 'Fuchsia']
-    cmaphid = colors.ListedColormap(hid_colors)
-    cmaphid.set_bad('white')
-    cmaphid.set_under('white')
-    #
-    radar_T,radar_z =  interpolate_sounding_to_radar(tfield_ref, alt_ref, radar)
-    #radar = add_field_to_radar_object(radar_T, radar, field_name='sounding_temperature')  
-    dzh_  = radar.fields['TH']['data']
-    dzv_  = radar.fields['TV']['data']
-    drho_ = radar.fields['RHOHV']['data']
-    dkdp_ = radar.fields['corrKDP']['data']
-    # Filters
-    ni = dzh_.shape[0]
-    nj = dzh_.shape[1]
-    for i in range(ni):
-        rho_h = drho_[i,:]
-        zh_h = dzh_[i,:]
-        for j in range(nj):
-            if (rho_h[j]<0.7) or (zh_h[j]<30):
-                dzh_[i,j]  = np.nan
-                dzv_[i,j]  = np.nan
-                drho_[i,j]  = np.nan
-                dkdp_[i,j]  = np.nan
-
-    scores          = csu_fhc.csu_fhc_summer(dz=dzh_[start_index:end_index], zdr=(dzh_-dzv_)[start_index:end_index] - opts['ZDRoffset'], 
-					     rho=drho_[start_index:end_index], kdp=dkdp_[start_index:end_index], 
-                                             use_temp=True, band='C', T=radar_T)
-    RHIs_nlev = np.argmax(scores, axis=0) + 1 
     #
     fig, axes = plt.subplots(nrows=1, ncols=1, constrained_layout=True, figsize=[13,12])
     pcm1 = axes.pcolormesh(lons, lats, RHIs_nlev, cmap = cmaphid, vmin=1.8, vmax=10.4)
@@ -2707,10 +2676,10 @@ def correct_PHIDP_KDP(radar, options, nlev, azimuth_ray, diff_value, tfield_ref,
     # AGREGAR HID?
     radar_T,radar_z =  interpolate_sounding_to_radar(tfield_ref, alt_ref, radar)
     radar = add_field_to_radar_object(radar_T, radar, field_name='sounding_temperature')  
-    dzh_  = radar.fields['TH']['data']
-    dzv_  = radar.fields['TV']['data']
-    drho_ = radar.fields['RHOHV']['data']
-    dkdp_ = radar.fields['corrKDP']['data']
+    dzh_  = radar.fields['TH']['data'].copy()
+    dzv_  = radar.fields['TV']['data'].copy()
+    drho_ = radar.fields['RHOHV']['data'].copy()
+    dkdp_ = radar.fields['corrKDP']['data'].copy()
     # Filters
     ni = dzh_.shape[0]
     nj = dzh_.shape[1]
@@ -2729,7 +2698,6 @@ def correct_PHIDP_KDP(radar, options, nlev, azimuth_ray, diff_value, tfield_ref,
                                              use_temp=True, band='C', T=radar_T)
     RHIs_nlev = np.argmax(scores, axis=0) + 1 
     radar.add_field_like('KDP','HID', RHIs_nlev, replace_existing=True)
-	
 	
     start_index = radar.sweep_start_ray_index['data'][nlev]
     end_index   = radar.sweep_end_ray_index['data'][nlev]
