da8ea3e6c41d547a7a14c177207d85845a465ba2
Victoria Sol Galligani <victoria.galligani@cima.fcen.uba.ar>
2022-12-01 16:53:44 -0300

    Update UpFolder.m

--- RT_simulations/UpFolder.m
+++ RT_simulations/UpFolder.m
@@ -1,1 +1,22 @@
 
+% FORMAT   paths = UpFolder(iz)
+%
+% Updates the wfolder according to iz
+%
+% OUT   paths  Updated paths
+% IN    nflag  Flagname specifying the output folder
+%              depending on the type of simulation
+%       paths  Structure with paths to files/folders
+%       iz     Name of the sub-folder
+%
+% 20.05.2020 Vasileios Barlakas
+%
+function paths = UpFolder(paths,iz)
+
+id             = num2str(iz);
+
+paths.wfolder = [paths.mfolder,'/',num2str(id)];
+
+if ~exist(paths.wfolder, 'dir')
+  mkdir(paths.wfolder);
+end
