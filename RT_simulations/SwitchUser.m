c443d01d34e0b94f15ed88d03c5f20e5cb4179c8
Victoria Sol Galligani <victoria.galligani@cima.fcen.uba.ar>
2022-12-01 16:55:00 -0300

    Update SwitchUser.m

--- RT_simulations/SwitchUser.m
+++ RT_simulations/SwitchUser.m
@@ -1,1 +1,24 @@
 
+% FORMAT   paths = SwitchUser(nflag)
+%
+% It automatically defines the user, but the user has
+% to manually set her/his working directories
+%
+% OUT   paths  Structure with paths to files/folders
+%	       depending on user
+% IN    nflag  Flagname specifying the output folder
+%              depending on the type of simulation
+%
+% 20.05.2020 Vasileios Barlakas
+%
+function paths = SwitchUser(nflag)
+
+%- Define user and paths
+%- Location of ARTS controlfiles
+paths.includes = '/home/victoria.galligani/Work/Software/ARTS/ARTS_032021/ARTS_032021/controlfiles/general';
+%- Main output path
+paths.mfolder  = [pwd,'/','Output','/',char(nflag)];
+%- Creates main output folder 
+if ~exist(paths.mfolder, 'dir')
+	mkdir(paths.mfolder);
+end
