15d7ce9f19c3e838c8596076109caa27f93c61e6
Vito Sol Galligani <victoria.galligani@cima.fcen.uba.ar>
2022-12-19 14:30:44 -0300

    Update CloudScenario.m

--- RT_simulations/CloudScenario.m
+++ RT_simulations/CloudScenario.m
@@ -16,9 +16,7 @@
 %
 % 01.01.2022 Adapted Vito Galligani
 %
-function [D] = CloudScenario(D,flag, hail_shape)
-
-cloud.fwhm = 1e3;
+function [D, C] = CloudScenario(D,flag, hail_shape)
 
 if flag == 1				 % Heavy Rain only
   cloud.cdh    = D.z_field.data;
@@ -214,16 +212,17 @@ elseif flag == 11                        % LR + GRAU (EXP. c)
 
 elseif flag == 12                       % Hail Only
 
- cloud_exp7 = cloud;
+ %cloud_exp7 = cloud;
  cloud_exp7.cdh    = D.z_field.data;
  cloud_exp7.cdh(:) = 0;
  hwc_min = 0;            % kg/m3 at j=18
  hwc_median = 0.25/2000; % kg/m3 at j=8 (0km)
  hwc_max = 0.4/1000;    % kg/m3 at j=0
- slope   = (0-D.z_field.data(17)) ./ (hwc_max-hwc_median);  %(y2-y1)/(x2-x1)
- for iz=1:18
-   cloud_exp7.cdh(iz) =  (D.z_field.data(iz) - D.z_field.data(18) )/slope;
- end
+ %slope   = (0-D.z_field.data(17)) ./ (hwc_max-hwc_median);  %(y2-y1)/(x2-x1)
+ %for iz=1:18
+ %  cloud_exp7.cdh(iz) =  (D.z_field.data(iz) - D.z_field.data(18) )/slope;
+ %end
+ cloud_exp7.cdh(1:5) =  0.2/1000;
 
  ice_rho = 917;
  d = 1e-2;
@@ -233,6 +232,39 @@ elseif flag == 12                       % Hail Only
 
  D.particle_bulkprop_field.data(1,:) = pnd;
  cloud.cdh = pnd; 
+
+ % scat_stuff
+
+ icerho  = 916; 
+ rhohail = 500; 
+ f_grid  = [10e9:1e9:170e9];
+ t_grid  = [210 240 273 283 293 303];
+ rfr_index_hail = zeros( length(f_grid), length(t_grid), 1 );
+ frahail = (rhohail-1) ./ (icerho-1);
+
+ % ICE
+ rfr_index.ice = zeros( length(f_grid), length(t_grid) );
+ for i = 1 : length(t_grid)
+  rfr_index_ice(:,i) = sqrt( eps_ice_matzler06( f_grid, t_grid(i) ) ).';
+ end
+ % HAIL
+ for i = 1 : length(t_grid)
+  [rfr_index_hail(:,i) rho(:,i)] = Maxwell_Gar_mix_rule(1, ...
+       rfr_index_ice(:,i), 1, 917, frahail);
+ end
+
+ C.SCAT_DATA = mie_arts_scat_data( f_grid, t_grid, rfr_index_hail, 0:1:180, d/2 );
+ C.SCAT_meta_DATA.version = 3;
+ C.SCAT_meta_DATA.description = '';
+ C.SCAT_meta_DATA.source = '';
+ C.SCAT_meta_DATA.refr_index = 'Truncated Matzler';
+ C.SCAT_meta_DATA.mass = nan;
+ C.SCAT_meta_DATA.diameter_max = nan;
+ C.SCAT_meta_DATA.diameter_volume_equ = nan;
+ C.SCAT_meta_DATA.diameter_area_equ_aerodynamical = nan;
+
+
+
 
 
 elseif flag == 13                        % Hail Only BOX
@@ -253,6 +285,9 @@ plot(cloud.cdh*1000, D.z_field.data/1e3, '-k','LineWidth',2);
 ylim([0 20])
 ylabel('Height (km)')
 title('Cloud profile')
+if flag == 12
+  xlabel('pnd_field')
+end
 
 
 end
