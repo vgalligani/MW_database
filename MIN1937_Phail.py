9548938d689f40164098556cc1befd87d052742c
Vito Sol Galligani <victoria.galligani@cima.fcen.uba.ar>
2023-03-03 15:45:14 -0300

    Update MIN1937_Phail.py

--- MIN1937_Phail.py
+++ MIN1937_Phail.py
@@ -48,14 +48,46 @@ plt.xlabel('MIN19PCT')
 plt.ylabel('MIN37PCT')
 fig.colorbar(im, label='P$_{hail}$ %')   
 
+# Figure 7 complement:
+fig_7=1
+if fig_7==1:
+    
+    # 09/02/2018
+    marker_in = '*'
+    plt.plot(234.4998, 165.5130, marker_in, color='k',  markersize=15, markerfacecolor='None') # coi=1 09/02/2018
+    plt.plot(262.4882, 212.6066, marker_in, color='k',  markersize=20, markerfacecolor='None') # 09/02/2018
+    plt.plot(273.0559, 232.7723, marker_in, color='k',  markersize=25, markerfacecolor='None') # 09/02/2018
+    plt.plot(281.8841, 260.8962, marker_in, color='k',  markersize=30, markerfacecolor='None') # 09/02/2018
+
+
+    #31/10/2019
+    marker_in = 'o'
+    plt.plot(237.4247, 151.6656, marker_in, color='k', markersize=15, markerfacecolor='None') # 31/10/2018
+    plt.plot(198.3740, 111.7183, marker_in, color='k', markersize=20, markerfacecolor='None') # 31/10/2018
+    plt.plot(244.4909, 174.1353, marker_in, color='k', markersize=25, markerfacecolor='None') # 31/10/2018
+    plt.plot(265.9133, 218.8043, marker_in, color='k', markersize=30, markerfacecolor='None') # 31/10/2018 (P=26.6%)
+
+    # 05/03/2019
+    plt.plot(249.9366, 164.4755, '',  markersize=15, markerfacecolor='None') # 05/03/2019 P=73.7%
+    plt.plot(271.2792, 238.1602, '',  markersize=15, markerfacecolor='None') # 05/03/2019 P=10.3%
+
+    # 02/09/2019
+    plt.plot(183.1349, 115.9271, '',  markersize=15, markerfacecolor='None') # 02/09/2019 P=98.9%
+    plt.plot(273.4064, 248.6457 , '',  markersize=15, markerfacecolor='None') # 02/09/2019 
+    plt.plot(276.8263, 252.4865, '',  markersize=15, markerfacecolor='None') # 02/09/2019
+    plt.plot(277.0152, 253.4831, '',  markersize=15, markerfacecolor='None') # 02/09/2019
+ 
+
+
+
+
+
 plt.plot(242.9253, 207.4241, 'kv', markersize=15, markerfacecolor='None') # coi2 08/02/2018
 plt.plot(270.2810, 238.8242, 'Pk', markersize=15, markerfacecolor='None') # coi1 08/02/2018
 
 #plt.plot(241.5902, 181.1631, '*k', markersize=20, markerfacecolor='None') # este estaba mal
 
-plt.plot(237.4247, 151.6656, 'dk', markersize=15, markerfacecolor='None') #
-plt.plot(198.3740, 111.7183, 'Dk', markersize=15, markerfacecolor='None') #
-plt.plot(244.4909, 174.1353, '^k', markersize=15, markerfacecolor='None') #
+
 
 plt.plot(249.4, 190.1, '',   markersize=15, markerfacecolor='None') # 11/11/2018 contour north
 plt.plot(273.5, 250.8, '',   markersize=15, markerfacecolor='None') # 11/11/2018 contour south
@@ -68,5 +100,9 @@ plt.plot(273.2686, 241.5902, , '',  markersize=15, markerfacecolor='None') # 15/
 
 
 
+ 
 
 
+
+
+    
